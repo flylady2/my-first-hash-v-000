a4ebde44dc15e96e420f90cc3274aa2623a11e81
Leslie Stevens <flylady@mail.utexas.edu>
2018-05-03 19:18:32 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,6 +36,7 @@ def adding
                  }
                  shipping_manifest["muskets"] => 2
                  shipping_manifest["gun powder"] => 4
+               end
                  shipping_manifest
   # add 2 muskets to the shipping_manifest hash below
 
